8916e50b919a5454ce980f5a235b737f10cd4dbf
John Henry Osa <johnhenryosa2@gmail.com>
2026-05-01 17:41:32 +0800

    viber chat

--- src/components/common/socialMedia/SocialMedia.jsx
+++ src/components/common/socialMedia/SocialMedia.jsx
@@ -8,19 +8,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const socialIcons = [
   { icon: faFacebookF, link: "https://www.facebook.com/jheno1st" },
   { icon: faLinkedin, link: "https://www.linkedin.com/in/jheno/" },
-  { icon: faViber, link: "viber://639055116752" },
+  { icon: faViber, link: "viber://chat?number=%2B639055116752" },
 ];
 
 const SocialMedia = () => {
   return socialIcons.map((item, index) => (
     <a
-      href={item.link}
-      className={`text-picto-primary hover:bg-picto-secondary p-2 pt-3 xs:p-2.5 xs:pt-3.75 sm:pt-4 md:pt-5 sm:p-3 md:p-3.75 hover:text-white rounded-md`}
       key={index}
+      href={item.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-picto-primary hover:bg-picto-secondary p-2 pt-3 xs:p-2.5 xs:pt-3.75 sm:pt-4 md:pt-5 sm:p-3 md:p-3.75 hover:text-white rounded-md"
     >
       <FontAwesomeIcon
         icon={item.icon}
-        className={`text-xl w-4.5 aspect-square`}
+        className="text-xl w-4.5 aspect-square"
       />
     </a>
   ));
